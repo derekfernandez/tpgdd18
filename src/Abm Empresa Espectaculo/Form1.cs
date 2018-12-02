5bf916d8b5f21015c4356bab7af57d059faf2a44
Derek F <derekfernandez182@gmail.com>
2018-12-01 23:30:12 -0300

    Revert "Agregado 1° ABM Empresa"
    This reverts commit 842ed056991e67ff45d92e6cd5f5a2672742b9eb.

--- src/Abm Empresa Espectaculo/Form1.cs
+++ src/Abm Empresa Espectaculo/Form1.cs
@@ -10,71 +10,11 @@ using System.Windows.Forms;
 
 namespace PalcoNet.Abm_Empresa_Espectaculo
 {
-    public partial class ABM_empresa_publicacion : Form
+    public partial class Form1 : Form
     {
-        public ABM_empresa_publicacion()
+        public Form1()
         {
             InitializeComponent();
         }
-
-        private void ABM_empresa_publicacion_Load(object sender, EventArgs e)
-        {
-
-        }
-
-
-        private void btnLimpiar_Click(object sender, EventArgs e)
-        {
-            foreach (Control c in this.Controls)
-            {
-
-                if (c is TextBox)
-                {
-
-                    c.Text = "";
-
-                }
-
-            }
-        }
-
-        private void btnAceptar_Click(object sender, EventArgs e)
-        {
-            //Funcionalidad de ingresar datos en la tabla empresa y validar que sean correctos
-            try
-            {
-                MessageBox.Show("Datos ingresados correctamente");
-            }
-            catch (Exception)
-            {
-                
-                throw;
-            }
-            
-        }
-
-        private void btnVolver_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                //Aca se va a llamar al Form anterior donde estan las opciones del admin
-                MessageBox.Show("Volviendo...");
-                this.Hide();
-                Form1 nuevaForm1 = new Form1();
-                nuevaForm1.Show();
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-
-        }
-
-      
-
-       
-
-     
     }
 }
