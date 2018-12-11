21a46dc1d08208fcfd7bb3183cafca5c4fe1619b
AgusMarquez97 <Aguusmarquez@gmail.com>
2018-12-11 01:19:39 -0300

    Arreglo Bugs + Avance Form Modificacion Empresa

--- data/T-SQLITO.sql
+++ data/T-SQLITO.sql
@@ -10,21 +10,24 @@ CREATE PROCEDURE [dbo].[pr_altaUsuario_empresa] (@empresaID INT, @empresaRazonSo
 AS
 BEGIN
 BEGIN TRY
-BEGIN TRANSACTION
 	DECLARE @username NVARCHAR(255), @password VARBINARY(255)
+	DECLARE @identity numeric(18,0)
+	SET @identity = (select count(*) from SQLITO.Usuarios)
 	--Asigno US and PW
+BEGIN TRANSACTION
 	SELECT @username = LOWER(SUBSTRING(@empresaRazonSocial,1,5)) + LOWER(SUBSTRING(@empresaRazonSocial,7,6)) + SUBSTRING(@empresaRazonSocial,17,2),@password = HASHBYTES('SHA2_256',CRYPT_GEN_RANDOM(8)) 
 	
 	INSERT INTO SQLITO.Usuarios (username, password, contraseniaActivada) VALUES (@username, @password, 0)
-	INSERT INTO SQLITO.Roles_Usuarios (rol_id, usuario_id)VALUES (1,SCOPE_IDENTITY())
+	INSERT INTO SQLITO.Roles_Usuarios (rol_id, usuario_id)VALUES (1,SCOPE_IDENTITY()) 
 
 COMMIT TRANSACTION
 	--Retorno el Id del usuario
 	RETURN (select COUNT(*) FROM SQLITO.Usuarios)
 END TRY
 BEGIN CATCH
-print ERROR_MESSAGE() 
-rollback transaction 
+rollback transaction
+DBCC CHECKIDENT ('SQLITO.Usuarios', RESEED,@identity);
+select ERROR_MESSAGE()  
 END CATCH
 END
 GO
@@ -43,15 +46,20 @@ CREATE proc [dbo].[pr_Alta_Empresa](@razonsocial nvarchar(255),
 as
 begin
 begin try
-begin transaction
 DECLARE @fecha_creacion datetime
 DECLARE @usuario_id int
 DECLARE @id_Empresa int
---Falta vincularlo con prUsuarioEmpresa
-IF EXISTS (select 1 from SQLITO.Empresas where razonsocial = @razonsocial) OR Exists(select 1 from SQLITO.Empresas where cuit = @cuit)
-begin
-PRINT 'Razon Social o CUIT no validos'
-end
+DECLARE @identity numeric(18,0)
+SET @identity = (select count(*) from SQLITO.Empresas)
+begin transaction
+IF EXISTS (select 1 from SQLITO.Empresas where razonsocial = @razonsocial)
+BEGIN
+raiserror ('Error: Razon Social ya registrada', 16, 15)
+END
+IF Exists(select 1 from SQLITO.Empresas where cuit = @cuit)
+BEGIN
+raiserror ('Error: CUIT ya registrado', 16, 15)
+END  
 SET @fecha_creacion = GETDATE()
 SET @id_Empresa =  (select COUNT(*) + 1 FROM SQLITO.Empresas) 
 EXEC @usuario_id = pr_altaUsuario_empresa @id_Empresa, @razonsocial
@@ -59,8 +67,9 @@ insert into [GD2C2018].[SQLITO].[Empresas](razonsocial,fecha_creacion,cuit,mail,
 commit transaction
 end try
 begin catch
-print ERROR_MESSAGE()
-rollback transaction 
+rollback transaction
+DBCC CHECKIDENT ('SQLITO.Empresas', RESEED, @identity);
+SELECT ERROR_MESSAGE() 
 end catch
 end
 GO
--- src/Abm Empresa Espectaculo/AMB_Modificar_Eliminar.cs
+++ src/Abm Empresa Espectaculo/AMB_Modificar_Eliminar.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using PalcoNet.Controllers;
+using PalcoNet.Misc;
+
+
 namespace PalcoNet.Abm_Empresa_Espectaculo
 {
     public partial class AMB_Modificar_Eliminar : Form
@@ -23,5 +27,23 @@ namespace PalcoNet.Abm_Empresa_Espectaculo
             ABM_Menu_Empresa nuevoMenu = new ABM_Menu_Empresa();
             nuevoMenu.Show();
         }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string query = string.Format("select * from SQLITO.Empresas where razonsocial='{0}' and cuit='{1}' and mail='{2}'", textBoxRazonSocial.Text, textBoxCUIT.Text, textBoxEmail.Text);
+                grillaEmpresas.DataSource = Database.ObtenerDataSet(query).Tables[0];
+            }
+            catch (Exception exp) 
+            {
+                MessageBox.Show("Error: " + exp.Message);
+            }
+        }
+
+    
+
+      
     }
 }
